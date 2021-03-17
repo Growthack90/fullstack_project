a5591d58624f1564f51167aed69cf2008a5dcd42
Growthack90 <hackmaker90@outlook.it>
2021-03-17 16:45:23 +0100

    apertura server con creazione di enpoint

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("ciaooooooo a tutti");
+const express = require("express");
+
+const app = express()
+
+//endpoint main
+app.get("/", (req, res) => {
+    res.status(200)
+    .send("Hello world!");
+})
+
+//second endpoint
+app.get("/europa", (req, res) => {
+    res.status(201)
+    .send("Hello Europe!");
+})
+
+//third endpoint
+app.get("/italia", (req, res) => {
+    res.status(202)
+    .send("Hello Italy!");
+})
+
+app.listen(3000, () => {
+    console.log("Hello developers!")
+})
