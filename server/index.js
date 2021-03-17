fd3bc2000a07ebafd927f18dbc2629289dbe3987
Growthack90 <hackmaker90@outlook.it>
2021-03-17 17:07:49 +0100

    aggiunto db interno a endpoint 2

--- server/index.js
+++ server/index.js
@@ -7,9 +7,17 @@ const app = express();
 app.use(helmet());
 
 //db frasi vangelo
+const fraseDelGiorno = [
+    "Chi vuol venire dietro di me prenda la sua croce e mi segua",
+    "Se vuoi essere perfetto, va, vendi quello che hai e seguimi",
+    "Non fare agli altri quello che non vuoi sia fatto a te",
+    "Fai agli altri quello che vuoi sia fatto a te",
+    "Non dare la perla ai porci"
+];
 const frasiVangelo = () => {
-    return "frase Vangelo";
-}
+    const indice = Math.floor(Math.random()*5);
+    return fraseDelGiorno [indice];
+};
 
 //endpoint main
 app.get("/", (req, res) => {
